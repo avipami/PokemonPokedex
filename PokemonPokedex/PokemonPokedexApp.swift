ba3e4622b4826ef155172c4313db1b82b71a0e38
Vincent <35411050+avipami@users.noreply.github.com>
2024-06-28 14:18:33 +0200

    Initial Commit

---
+++ PokemonPokedex/PokemonPokedexApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokemonPokedexApp.swift
+//  PokemonPokedex
+//
+//  Created by Vincent Palma on 2024-06-28.
+//
+
+import SwiftUI
+
+@main
+struct PokemonPokedexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
